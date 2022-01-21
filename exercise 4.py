52e2a13ffbc538d066577075650ad2e58ada45af
Daniel !Gao-!gaseb <91456983+Agabe-gaogaseb07@users.noreply.github.com>
2022-01-21 21:51:21 +0200

    Exercise 4 on Module.
    The aim is to switch variables  A and B.

---
+++ exercise 4.py
@@ -0,0 +1,17 @@
+#First prompts to enter a value.
+a = input("a: ")
+b = input("b: ")
+
+#Now as a = a, the value remains on the same variable.
+#But if you switch the variables ("a = " + a) to ("a = " + b), it will switch the value on variable (a) to the value on variable (b).
+print("a = " + a)
+print("b = " + b)
+
+#Like illustrated below with this in the second code!!
+
+#This print function is my underlining.
+print("/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n/n")
+
+#This is the second implementatation of the code.
+print("a = " + b)
+print("b = " + a)
